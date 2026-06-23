43a8c81d55f786b1ec10dd69653162d8762842f6
jayaglobaltech112 <jayaglobaltech112@gmail.com>
2026-06-23 14:52:10 +0530

    Configure Django backend for Render deployment

--- backend/settings.py
+++ backend/settings.py
@@ -39,7 +39,7 @@ TEXTBEE_DEVICE_ID = os.environ.get('TEXTBEE_DEVICE_ID', '')
 SECRET_KEY = 'django-insecure-3samcu@3*%snr)*lwh0k$b$d1b8(m7zu%eln02erf+&6fh4t(o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
 ALLOWED_HOSTS = ['*']
 
@@ -66,6 +66,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware', # Put at the top
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -162,6 +163,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
